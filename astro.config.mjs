e37a8d716626157188ec90ff8b733e7cf4060686
Surya Avala <suryaavala.dev@gmail.com>
2023-12-04 20:48:16 +1100

    fix: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://suryaavala.com/ah_chambers_of_law.github.io',
+  site: 'https://ahchambersoflaw.in',
   
 });
